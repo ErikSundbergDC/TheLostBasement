a35852ff7ae68fbe2b0d55b7453e6c937be7fb1b
ErikSundbergDC <113100974+ErikSundbergDC@users.noreply.github.com>
2026-09-02 15:49:37 +0200

    Add project files.

---
+++ TheLostBasement/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace TheLostBasement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Room> rooms = new List<Room>();
+
+            rooms.Add(new Room("Trappan", "En mörk trapp som leder neråt."));
+            rooms.Add(new Room("Ett mörkt rum", "Här nere i den mörka källaren ser du ingenting eftersom du inte har hittat den hemliga lyktan ännu."));
+            rooms[0].North = rooms[1];
+           
+
+            foreach (Room room in rooms)
+            {
+                Console.WriteLine(room.Name);
+                Console.WriteLine(room.Description);
+                if(room.North != null)
+                {
+                    Console.WriteLine("Går du norrut kommer du till " +
+                                        room.North.Name);
+                }
+                
+                Console.WriteLine();
+            }
+         
+
+
+        }
+    }
+}
---
+++ TheLostBasement/Room.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheLostBasement
+{
+    internal class Room
+    {
+        public string Name { get; private set; }
+        public string Description { get; set; }
+
+        public Room North { get; set; }
+
+        public Room(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+    }
+}
