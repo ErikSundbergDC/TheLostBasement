900d0a412a21ee7f7244728bd8ca9daf80219eb8
ErikSundbergDC <113100974+ErikSundbergDC@users.noreply.github.com>
2026-09-09 15:52:39 +0200

    Några fler roliga rum.

--- TheLostBasement/Program.cs
+++ TheLostBasement/Program.cs
@@ -8,8 +8,14 @@
 
             rooms.Add(new Room("Trappan", "En mörk trapp som leder neråt."));
             rooms.Add(new Room("Ett mörkt rum", "Här nere i den mörka källaren ser du ingenting eftersom du inte har hittat den hemliga lyktan ännu."));
+            rooms.Add(new Room("Ett mörkare rum", "Här nere i den mörka källaren ser du ingenting eftersom du inte har hittat den hemliga lyktan ännu."));
+            rooms.Add(new Room("Mörkaste rummet", "Här nere i den mörka källaren ser du ingenting eftersom du inte har hittat den hemliga lyktan ännu."));
             rooms[0].North = rooms[1];
             rooms[1].South = rooms[0];
+            rooms[1].West = rooms[2];
+            rooms[2].East = rooms[1];
+            rooms[2].South = rooms[3];
+            rooms[3].North = rooms[2];
 
             PlayerCharacter playerCharacter = new PlayerCharacter("Erik");
             playerCharacter.Position = rooms[0];
